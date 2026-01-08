4d06a4563b3f7bed792a4949cfb8d45ef4024c3b
Ronan Messuwe <ronan@kanta.fr>
2026-01-08 10:19:09 +0100

    refactor(IDisplay): allows the destruction of the children's class

--- src/traffic_light/IDisplay.h
+++ src/traffic_light/IDisplay.h
@@ -2,6 +2,7 @@
 
 class IDisplay {
   public:
+    virtual ~IDisplay() = default; // Permet de détruire la classe fille SevenSegmentBCDDisplay si on détruit la classe parente IDisplay.
     virtual void begin() = 0;
     virtual void show(uint8_t value) = 0;
 };
